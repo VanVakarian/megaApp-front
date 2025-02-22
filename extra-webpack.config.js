4fdc06aede7fa80e1ad637b1c49fba023972eab2
VanVakarian <void@vslav.dev>
2025-02-23 00:23:53 +0300

    🔊 [082] latest commit info log improved

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const childProcess = require('child_process');
 const fs = require('fs');
+const https = require('https');
 
 const getGitInfo = () => {
   try {
@@ -11,11 +12,41 @@ const getGitInfo = () => {
   }
 };
 
+const getCommitDateTime = () => {
+  try {
+    const timestamp = childProcess.execSync('git show -s --format=%ci HEAD').toString().trim();
+    return new Date(timestamp).toLocaleString('ru-RU', { dateStyle: 'short', timeStyle: 'medium' });
+  } catch (e) {
+    return 'unknown';
+  }
+};
+
+// making a file in assets with latest commit info
 class BuildInfoPlugin {
-  // making a file in assets with frontend app build info
   apply(compiler) {
     compiler.hooks.done.tap('BuildInfoPlugin', () => {
-      const buildInfo = `console.log("Commit: [${getGitInfo()}] built at [${new Date().toLocaleTimeString()}]");`;
+      const buildInfo = `
+        const frontInfo = {
+          commitHash: "${getGitInfo()}",
+          commitDateTime: "${getCommitDateTime()}"
+        };
+
+        let backendInfo = {
+          commitHash: "unknown",
+          commitDateTime: "unknown"
+        };
+
+        fetch('/api/debug/commit-info')
+          .then(response => response.json())
+          .then(data => {
+            backendInfo = data;
+          })
+          .catch(() => {})
+          .finally(() => {
+            console.log("Latest front commit: [" + frontInfo.commitHash + ", " + frontInfo.commitDateTime + "]");
+            console.log("Latest back commit: [" + backendInfo.commitHash + ", " + backendInfo.commitDateTime + "]");
+          });
+      `;
       fs.writeFileSync('./src/assets/build-info.js', buildInfo);
     });
   }
