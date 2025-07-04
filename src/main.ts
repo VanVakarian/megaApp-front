418cb549dc8c7e0c5fc1fc02b6c9229aacf3b95e
VanVakarian <void@vslav.dev>
2025-07-04 22:30:55 +0300

    🔧 [096] 17 -> 18

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,7 +19,6 @@ bootstrapApplication(MainAppComponent, {
     provideRouter(routes),
     { provide: LocationStrategy, useClass: HashLocationStrategy },
     importProvidersFrom(
-      HttpClientModule,
       BrowserAnimationsModule,
       JwtModule.forRoot({
         config: {
